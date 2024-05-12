f9d7a6bde94554da043db5e9301f9575fd597dd8
notness <inessa.fedorova.01@mail.ru>
2024-05-12 13:07:16 +0700

    update dag, move airflow credentials to .env file

--- src/test_dag.py
+++ src/test_dag.py
@@ -1,46 +1,46 @@
 import joblib
 import pandas as pd
 import logging
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import classification_report
 
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 
-def read_csv(file_path):
-    data = pd.read_csv(file_path)
-    return data
+def read_csv(train_path, test_path):
+    train = pd.read_csv(train_path)
+    test = pd.read_csv(test_path)
+
+    return train, test
 
 
 def preprocess_data(target_column, **kwargs):
     task_instance = kwargs['ti']
 
-    data = task_instance.xcom_pull(task_ids="read_csv")
-    data = data.dropna()
+    data_train, data_test = task_instance.xcom_pull(task_ids="read_csv")
+    data_train, data_test = data_train.dropna(), data_test.dropna()
 
-    X = data.drop(target_column, axis=1)
-    y = data[target_column]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test = data_train.drop(target_column, axis=1), data_test.drop(target_column, axis=1)
+    y_train, y_test = data_train[target_column], data_test[target_column]
 
-    return X_train.to_dict(), X_test.to_dict(), y_train.to_dict(), y_test.to_dict()
+    return X_train.to_dict(), y_train.to_dict(), X_test.to_dict(), y_test.to_dict()
 
 
-def train_linear_regression(**kwargs):
+def train_model(**kwargs):
     task_instance = kwargs['ti']
 
-    X_train_dict, _, y_train_dict, _ = task_instance.xcom_pull(task_ids="preprocess_data")
+    X_train_dict, y_train_dict, _, _ = task_instance.xcom_pull(task_ids="preprocess_data")
     logging.warning(X_train_dict)
 
     X_train = pd.DataFrame.from_dict(X_train_dict)
     y_train = pd.Series(y_train_dict)
 
-    model = LinearRegression()
+    model = GradientBoostingClassifier(n_estimators=500, learning_rate=0.8, random_state=27, max_depth=6)
     model.fit(X_train, y_train)
 
-    model_filepath = "/opt/airflow/data/linear_regression_model.pkl"
+    model_filepath = "/opt/airflow/data/model.pkl"
     joblib.dump(model, model_filepath)
 
     return model_filepath
@@ -49,18 +49,18 @@ def train_linear_regression(**kwargs):
 def test_model(**kwargs):
     task_instance = kwargs['ti']
 
-    model_filepath = task_instance.xcom_pull(task_ids="train_linear_regression")
+    model_filepath = task_instance.xcom_pull(task_ids="train_model")
 
     model = joblib.load(model_filepath)
 
-    _, X_test_dict, _, y_test_dict = task_instance.xcom_pull(task_ids="preprocess_data")
+    _, _, X_test_dict, y_test_dict = task_instance.xcom_pull(task_ids="preprocess_data")
 
     X_test = pd.DataFrame.from_dict(X_test_dict)
     y_test = pd.Series(y_test_dict)
 
     y_pred = model.predict(X_test)
-    mse = mean_squared_error(y_test, y_pred)
-    return mse
+    report = classification_report(y_test, y_pred)
+    return report
 
 
 default_args = {
@@ -70,24 +70,24 @@ default_args = {
     "email_on_retry": False,
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
-    "start_date": datetime(2023, 5, 3),
+    "start_date": datetime(2024, 5, 3),
 }
 
 dag = DAG(
-    "linear_regression_pipeline",
+    "classification_pipeline",
     default_args=default_args,
-    description="A pipeline to read CSV, preprocess data, train and test a linear regression model",
+    description="A pipeline to read CSV, preprocess data, train and test a gradient boosting classification model",
     schedule_interval=timedelta(days=1),
     catchup=False,
 )
 
-file_path = "/opt/airflow/data/train.csv"
-target_column = "y"
+train_path, test_path = "/opt/airflow/data/train.csv", "/opt/airflow/data/test.csv"
+target_column = "class"
 
 t1 = PythonOperator(
     task_id="read_csv",
     python_callable=read_csv,
-    op_args=[file_path],
+    op_args=[train_path, test_path],
     dag=dag,
 )
 
@@ -100,8 +100,8 @@ t2 = PythonOperator(
 )
 
 t3 = PythonOperator(
-    task_id="train_linear_regression",
-    python_callable=train_linear_regression,
+    task_id="train_model",
+    python_callable=train_model,
     provide_context=True,
     dag=dag,
 )
